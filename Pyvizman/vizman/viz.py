274039e159b31c795fe133403ea5a4f22d6d11fb
Kayson Fakhar <kayson.fakhar@gmail.com>
2024-08-05 19:43:11 +0200

    added some colormaps

--- Pyvizman/vizman/viz.py
+++ Pyvizman/vizman/viz.py
@@ -1,9 +1,12 @@
 import json
-import seaborn as sns
-import matplotlib.pyplot as plt
 from typing import Tuple
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
 from matplotlib import font_manager
-
+import numpy as np
+import pandas as pd
+from cycler import cycler
 
 def load_data_from_json(file_path: str) -> dict:
     """Just loads colors (and other data) from a json file.
@@ -18,6 +21,7 @@ def load_data_from_json(file_path: str) -> dict:
         data = json.load(file)
     return data
 
+
 def add_font(font_path: str) -> None:
     """Adds a font family to the matplotlib font manager. If you only have one font then it just adds that one.
 
@@ -27,8 +31,10 @@ def add_font(font_path: str) -> None:
     for font in font_manager.findSystemFonts(font_path):
         font_manager.fontManager.addfont(font)
 
+
 def set_visual_style(json_color_path: str='Pyvizman/vizman/colors.json',
-                     font_family:str="Atkinson Hyperlegible") -> None:
+                     font_family:str="Atkinson Hyperlegible",
+                     change_colors:bool=True) -> None:
     """Sets the visual style for the plots. 
     This is basically my taste so feel free to change things (the fonts, e.g.) to your liking.
     But the idea is that the fonts are compatible with some other applications in case you want to edit the pdf file later.
@@ -45,9 +51,12 @@ def set_visual_style(json_color_path: str='Pyvizman/vizman/colors.json',
             I chose these because they are open source and they are extremely readable.
             By default it tries to load Atkinson Hyperlegible.
             If you want to use another font you can add it to the font folder and call `add_font`.
-            Honestly, adding fonts is a pain in the ass so it might not work! But if that happens, it just uses its own default font.
+            Honestly, adding fonts is a pain in the ass so it might not work! 
+            But if that happens, it just uses its own default font.
+        change_colors (bool): changes the default color cycler to something less ugly!
     """
     colors = load_data_from_json(json_color_path)
+
     sns.set_theme(
         style="ticks",
         font_scale=1,
@@ -85,11 +94,50 @@ def set_visual_style(json_color_path: str='Pyvizman/vizman/colors.json',
             "axes.edgecolor": colors['neutrals']['HALF_BLACK'],
             "axes.labelcolor": colors['neutrals']['HALF_BLACK'],
             "xtick.color": colors['neutrals']['HALF_BLACK'],
-            "ytick.color": colors['neutrals']['HALF_BLACK'],
+            "ytick.color": colors['neutrals']['HALF_BLACK'],          
         },
     )
+    if change_colors:
+        cycling_colors = [colors["neutrals"]["OLIVE_GRAY"],
+                        colors["colds"]["LAKE_BLUE"],
+                        colors["warms"]["ORANGE"],
+                        colors["purples"]["PURPLE"],
+                        colors["greens"]["SLOW_GREEN"],
+                        colors["neutrals"]["HALF_BLACK"],
+                        colors["colds"]["INK_BLUE"],
+                        colors["warms"]["LECKER_RED"],
+                        colors["purples"]["PINK"],
+                        colors["greens"]["LUXUARY_GREEN"],
+                        colors["neutrals"]["GRAY"],
+                        colors["colds"]["TEAL"],
+                        colors["warms"]["YELLOW"],
+                        colors["purples"]["PURPLER"],]
+        matplotlib.rcParams["axes.prop_cycle"]= cycler(color=cycling_colors)
 
 
+
+def give_colormaps(json_color_path: str='Pyvizman/vizman/colors.json') -> dict:
+    colors = load_data_from_json(json_color_path)
+
+    colormaps = {
+        "divergents":{
+            "db_bw_lr":sns.blend_palette([colors["colds"]["DEEP_BLUE"],
+                                           colors["neutrals"]["BONE_WHITE"],
+                                           colors["warms"]["LECKER_RED"]],
+                                          as_cmap=True),
+            "nb_bw_dr":sns.blend_palette([colors["colds"]["NIGHT_BLUE"],
+                                           colors["neutrals"]["BONE_WHITE"],
+                                           colors["warms"]["DEEP_RED"]],
+                                          as_cmap=True),
+            "sg_bw_pi":sns.blend_palette([colors["greens"]["SLOW_GREEN"],
+                                           colors["neutrals"]["BONE_WHITE"],
+                                           colors["purples"]["PINK"]],
+                                          as_cmap=True)},
+        "monos":{"bw_lr":sns.blend_palette([colors["neutrals"]["BONE_WHITE"],
+                                           colors["warms"]["LECKER_RED"]],
+                                          as_cmap=True),}
+        }
+    return colormaps
 
 def inch_to_cm(figure_size_in_inch: Tuple[float, float]) -> Tuple[float, float]:
     """Converts figure size from inches to centimeters.
@@ -104,6 +152,7 @@ def inch_to_cm(figure_size_in_inch: Tuple[float, float]) -> Tuple[float, float]:
 
 
 def cm_to_inch(figure_size_in_cm: Tuple[float, float]) -> Tuple[float, float]:
+
     """Converts figure size from centimeters to inches.
 
     Args:
@@ -113,3 +162,27 @@ def cm_to_inch(figure_size_in_cm: Tuple[float, float]) -> Tuple[float, float]:
         float: Tuple[float, float]. figure size in inches (x,y).
     """
     return tuple(map(lambda x: x / 2.54, figure_size_in_cm))
+
+
+def plot_adjmat(adjmat:np.ndarray|pd.DataFrame,
+                axis:matplotlib.axes = None,
+                sns_kwargs:dict=None) -> None:
+    sns_kwargs = sns_kwargs or {}
+    if axis is None:
+        axis = plt.figure().add_axes([0,0,1,1])
+    
+    if np.any(adjmat<0):
+        sns.heatmap(adjmat, ax=axis, center = 0, cbar=False, square=True,**sns_kwargs)
+    else:
+        sns.heatmap(adjmat, ax=axis, cbar=False, square=True,**sns_kwargs)
+    
+    pos = axis.get_position()
+    height = pos.height
+    left = pos.x0 + pos.width + 0.03
+    bottom = pos.y0
+    
+    # Create a color bar manually, matching the height of the heatmap
+    cax = plt.gcf().add_axes([left, bottom, 0.03, height])
+    plt.colorbar(axis.collections[0], cax=cax)
+    sns.despine(top=False, right=False, left=False, bottom=False)
+    return axis
